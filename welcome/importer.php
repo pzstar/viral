1abcf33dfa04c87251a1bd0d368e4e49a2249b5b
prabin <prabin@localhost>
2020-02-16 15:21:12 +0545

    Merge origin/master
    Conflicts:
    	.DS_Store

--- welcome/importer.php
+++ welcome/importer.php
@@ -8,11 +8,11 @@ function viral_import_files() {
     return array(
         array(
             'import_file_name' => 'Viral Demo Import',
-            'import_file_url' => 'https://hashthemes.com/import-files/viral/viral.xml',
-            'import_widget_file_url' => 'https://hashthemes.com/import-files/viral/viral-widgets.wie',
-            'import_customizer_file_url' => 'https://hashthemes.com/import-files/viral/viral-customizer.dat',
-            'import_preview_image_url' => 'https://i0.wp.com/themes.svn.wordpress.org/viral/1.3.1/screenshot.png',
-            'preview_url' => 'http://demo.hashthemes.com/viral'
+            'local_import_file' => trailingslashit(get_template_directory()) . 'welcome/demo-data/content.xml',
+            'local_import_widget_file' => trailingslashit(get_template_directory()) . 'welcome/demo-data/widgets.wie',
+            'local_import_customizer_file' => trailingslashit(get_template_directory()) . 'welcome/demo-data/customizer.dat',
+            'import_preview_image_url' => 'https://i0.wp.com/themes.svn.wordpress.org/square/1.4.0/screenshot.png',
+            'preview_url' => 'https://demo.hashthemes.com/viral'
         )
     );
 }
@@ -22,11 +22,12 @@ add_filter('pt-ocdi/import_files', 'viral_import_files');
 function viral_after_import_setup() {
     // Assign menus to their locations.
     $main_menu = get_term_by('name', 'Primary Menu', 'nav_menu');
+    $top_menu = get_term_by('name', 'Top Header Menu', 'nav_menu');
 
     set_theme_mod('nav_menu_locations', array(
         'primary' => $main_menu->term_id,
-            )
-    );
+        'top-menu' => $top_menu->term_id
+    ));
 
     // Assign front page and posts page (blog page).
     $front_page_id = get_page_by_title('Home Page');
