ca3dcc7b74efa1de8f304ef070c491e5c261782b
prabin <prabin@macbook-pro.domain.name>
2023-04-09 16:18:23 +0545

    update

--- inc/customizer/customizer-functions.php
+++ inc/customizer/customizer-functions.php
@@ -131,3 +131,11 @@ function viral_is_upgrade_notice_active() {
     $show_upgrade_notice = apply_filters('viral_hide_upgrade_notice', get_theme_mod('viral_hide_upgrade_notice', false));
     return !$show_upgrade_notice;
 }
+
+function viral_check_cfu(){
+    if(class_exists('Hash_Custom_Font_Uploader')){
+        return false;
+    }else{
+        return true;
+    }
+}
