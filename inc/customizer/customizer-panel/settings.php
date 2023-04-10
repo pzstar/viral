cc74d1bbe55507f2d657fe4c013e5683029f34c0
prabin <prabin@macbook-pro.domain.name>
2023-04-10 15:24:15 +0545

    Update settings.php

--- inc/customizer/customizer-panel/settings.php
+++ inc/customizer/customizer-panel/settings.php
@@ -429,7 +429,7 @@ $wp_customize->add_section(new Viral_Upgrade_Section($wp_customize, 'viral-hcfu-
     ),
     'upgrade_text' => esc_html__('Purchase Custom Font Uploader', 'viral'),
     'upgrade_url' => 'https://hashthemes.com/checkout?edd_action=add_to_cart&download_id=48193',
-    'viral_check_cfu' => 'viral_check_cfu'
+    'active_callback' => 'viral_check_cfu'
 )));
 
 /* ============HEADER SETTING PANEL============ */
