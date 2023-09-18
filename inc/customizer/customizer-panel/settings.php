d0f95c594029700685c2e65f76652ffdbfae4904
prabin <prabin@macbook-pro.domain.name>
2023-09-18 18:10:51 +0545

    Update settings.php

--- inc/customizer/customizer-panel/settings.php
+++ inc/customizer/customizer-panel/settings.php
@@ -447,7 +447,7 @@ $wp_customize->add_section(new Viral_Upgrade_Section($wp_customize, 'viral-hcfu-
         esc_html__('Upload custom fonts. The uploaded font will display in the typography font family list.', 'viral'),
     ),
     'upgrade_text' => esc_html__('Purchase Custom Font Uploader', 'viral'),
-    'upgrade_url' => 'https://hashthemes.com/checkout?edd_action=add_to_cart&download_id=48193',
+    'upgrade_url' => 'https://hashthemes.com/downloads/hash-custom-font-uploader/',
     'active_callback' => 'viral_check_cfu'
 )));
 
